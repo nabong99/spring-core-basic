ff5c9e6738a88936ff5709cb48c4889e8e28fc92
nabong99 <skgusslarj@naver.com>
2022-05-03 00:50:09 +0900

    스프링 핵심 원리 이해1 - 예제 만들기 - 회원 도메인 실행과 테스트

---
+++ src/main/java/hello/democore/MemberApp.java
@@ -0,0 +1,18 @@
+package hello.democore;
+
+import hello.democore.member.Grade;
+import hello.democore.member.Member;
+import hello.democore.member.MemberService;
+import hello.democore.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberServiceImpl memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
