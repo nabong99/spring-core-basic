a76f080f5e63b000a904b87823a5ea85421815b8
nabong99 <skgusslarj@naver.com>
2022-05-03 00:06:43 +0900

    스프링 핵심 원리 이해1 -예제 만들기 - 프로젝트 생성

---
+++ src/test/java/hello/democore/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.democore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
