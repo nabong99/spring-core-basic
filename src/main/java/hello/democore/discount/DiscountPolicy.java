06442c32e873998376cc461b8a96eb948e2ccf33
nabong99 <skgusslarj@naver.com>
2022-05-03 10:01:59 +0900

    스프링 핵심 원리 이해1 - 예제 만들기 - 주문과 할인 도메인 개발

---
+++ src/main/java/hello/democore/discount/DiscountPolicy.java
@@ -0,0 +1,10 @@
+package hello.democore.discount;
+
+import hello.democore.member.Member;
+
+public interface DiscountPolicy {
+    /*
+    @return 할인 대상 금액
+     */
+    int discount(Member member, int price);
+}
---
+++ src/main/java/hello/democore/order/OrderServiceImpl.java
@@ -0,0 +1,23 @@
+package hello.democore.order;
+
+
+import hello.democore.discount.DiscountPolicy;
+import hello.democore.discount.FixDiscountPolicy;
+import hello.democore.member.Member;
+import hello.democore.member.MemberRepository;
+import hello.democore.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member,itemPrice);
+
+        return new Order(memberId, itemName, itemPrice,discountPrice);
+    }
+}
