30d76aae63d0fd3c9d20a4f05b01067bcec7e49b
nabong99 <skgusslarj@naver.com>
2022-05-03 11:59:07 +0900

    스프링 핵심 원리 이해2 - 객체 지향 원리 적용 - 새로운 할인 정책 적용과 문제점

--- src/main/java/hello/democore/order/OrderServiceImpl.java
+++ src/main/java/hello/democore/order/OrderServiceImpl.java
@@ -3,6 +3,7 @@ package hello.democore.order;
 
 import hello.democore.discount.DiscountPolicy;
 import hello.democore.discount.FixDiscountPolicy;
+import hello.democore.discount.RateDiscountPolicy;
 import hello.democore.member.Member;
 import hello.democore.member.MemberRepository;
 import hello.democore.member.MemoryMemberRepository;
@@ -10,8 +11,9 @@ import hello.democore.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
